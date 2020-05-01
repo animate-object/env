4c2d61a4dc439e67158acac153eb89747bc666e4
Alex <alex@physera.com>
2020-04-30 19:19:19 -0500

    more osx support

--- profile/secondary/prompt.sh
+++ profile/secondary/prompt.sh
@@ -1,1 +1,7 @@
-PS1="\w> "
+echo $SHELL
+if [[ $SHELL = *zsh ]]; then
+   echo 'zsh'
+   PS1="%1d>"
+else
+   PS1="\w>"
+fi 
--- setup/utils.sh
+++ setup/utils.sh
@@ -4,7 +4,8 @@ if [[ "$OS" == "DEFAULT" ]]; then
 	sudo apt update
 	sudo apt install tree vim curl awscli jq
 elif [[ "$OS" == "OSX" ]]; then
-	brew install tree curl jq
+	brew install tree curl jq awscli
+    brew cask install sequel-pro
 else
         echo "OS $OS not supported"
 fi
