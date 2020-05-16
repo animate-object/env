d75f2024ed717a3e773c22a69b11322b27b7fea2
animate-object <alexander.f.prinsen@gmail.com>
2020-05-16 13:22:22 -0500

    little linux bug fix

--- profile/foundational/env.sh
+++ profile/foundational/env.sh
@@ -2,7 +2,7 @@ export WORKSPACE="$HOME/workspace"
 export HISTSIZE=100000
 export SAVEHIST=$HISTSIZE
 
-export OS="$([[ $(uname) == "Darwin" ]] && echo 'OSX' || 'DEFAULT')"
+export OS="$([[ $(uname) == "Darwin" ]] && echo 'OSX' || echo 'DEFAULT')"
 export PATH="/usr/local/opt/ruby/bin:$PATH"
 export PATH=$PATH:~/Library/Android/sdk/tools:~/Library/Android/sdk/platform-tools
 
